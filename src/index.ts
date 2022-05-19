83149e91e3a2a69260bb949d9a4de27573714684
MarceloA22 <104115950+MarceloA22@users.noreply.github.com>
2022-05-19 18:35:55 -0300

    se armo el metodo cantidadDeDivisores

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 let data1: number = Number(prompt("Ingrese un número entero"));
 let data2: number = 1;
 let vuelta: number = 0;
-for (data2; data2 <= data1; data2++) {
-  if (data1 % data2 === 0) {
-    vuelta = data2 / data2 + vuelta * 1;
+
+function cantidadDeDivisores() {
+  for (data2; data2 <= data1; data2++) {
+    if (data1 % data2 === 0) {
+      vuelta = data2 / data2 + vuelta * 1;
+    }
   }
+  console.log("El número " + data1 + " tiene " + vuelta + " divisores");
 }
-console.log("El número " + data1 + " tiene " + vuelta + " divisores");
+cantidadDeDivisores();
